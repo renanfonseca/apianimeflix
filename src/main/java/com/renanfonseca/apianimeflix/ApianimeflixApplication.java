7188ca7645a94d655afb074f142a86e207591776
Renan Fonseca <marcos.renan.cf@gmail.com>
2022-11-10 14:27:56 -0300

    initial commit

---
+++ src/main/java/com/renanfonseca/apianimeflix/ApianimeflixApplication.java
@@ -0,0 +1,13 @@
+package com.renanfonseca.apianimeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApianimeflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApianimeflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanfonseca/apianimeflix/ApianimeflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanfonseca.apianimeflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApianimeflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
